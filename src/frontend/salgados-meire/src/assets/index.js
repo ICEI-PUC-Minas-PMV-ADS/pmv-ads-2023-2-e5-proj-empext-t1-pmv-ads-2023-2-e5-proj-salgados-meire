c3aa01fa98e6d167fa5ffbab98fe021da628655f
Raíssa Queiroz <gnr0034@gmail.com>
2023-09-23 13:07:06 -0300

    add images

---
+++ src/frontend/salgados-meire/src/assets/index.js
@@ -0,0 +1,15 @@
+import coxinha from "../assets/coxinha.svg";
+import croissant from "../assets/croissant.svg";
+import empada_frango from "../assets/empada_frango.svg";
+import enroladinho_salsicha from "../assets/enroladinho_salsicha.svg";
+import kibe from "../assets/kibe.svg";
+import pastel_assado from "../assets/pastel_assado.svg";
+
+export {
+  coxinha,
+  croissant,
+  empada_frango,
+  enroladinho_salsicha,
+  kibe,
+  pastel_assado,
+};
