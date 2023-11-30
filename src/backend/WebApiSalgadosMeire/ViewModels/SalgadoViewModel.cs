9b39fd91081ea3f5179a09750e6743eaf39b9faf
Raíssa Queiroz <gnr0034@gmail.com>
2023-11-30 10:23:02 -0300

    view models

---
+++ src/backend/WebApiSalgadosMeire/ViewModels/SalgadoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiSalgadosMeire.ViewModels
+{
+    public class SalgadoViewModel
+    {
+        public Guid? Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public double ValorUnitário { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
