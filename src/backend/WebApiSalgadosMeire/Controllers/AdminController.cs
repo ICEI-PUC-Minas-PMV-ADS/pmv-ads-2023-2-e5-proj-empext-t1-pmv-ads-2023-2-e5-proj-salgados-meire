05b95a7f03f78b87c9dba6f206391e607e4f819e
Raíssa Queiroz <gnr0034@gmail.com>
2023-11-30 10:21:34 -0300

    controllers

---
+++ src/backend/WebApiSalgadosMeire/Controllers/AdminController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiSalgadosMeire.Moldels;
+using WebApiSalgadosMeire.Services;
+using WebApiSalgadosMeire.ViewModels;
+
+namespace WebApiSalgadosMeire.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly SalgadosServices _salgadosServices;
+        private readonly PedidoService _pedidoService;
+
+        public AdminController(SalgadosServices salgadosServices, PedidoService pedidoService)
+        {
+            _salgadosServices = salgadosServices;
+            _pedidoService = pedidoService;
+        }
+
+        [HttpGet]
+        [Route("obter-salgados-ativos")]
+        public async Task<List<Salgado>> ObterSalgadosAtivos()
+        {
+            return await _salgadosServices.ObterTodosSalgados();
+        }
+
+        [HttpPost]
+        [Route("adicionar-salgado")]
+        public async Task<IActionResult> AdicionarSalgado(SalgadoViewModel model)
+        {
+            var novoSalgado = new Salgado(model.Nome, model.Descricao, model.ValorUnitário, model.ImageUrl);
+
+            await _salgadosServices.AdicionarSalgado(novoSalgado);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("atualizar-salgado/{id:Guid}")]
+        public async Task<IActionResult> AtualizarSalgado(Guid id, SalgadoViewModel model)
+        {
+            var salgado = await _salgadosServices.ObterSalgadoPorId(id);
+
+            if (salgado == null) return NotFound("Salgado não encontrado");
+
+            salgado.AtualizarSalgado(model.Nome, model.Descricao, model.ValorUnitário, model.ImageUrl);
+
+            await _salgadosServices.AtualizarSalgado(salgado);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("obter-relatorio/{mes:int}")]
+        public async Task<IActionResult> ObterRelatorioPorMes(int mes)
+        {
+            var pedidos = await _pedidoService.ObterPedidosPorMes(2023, mes);
+
+            return Ok(pedidos);
+        }
+
+        [HttpPost]
+        [Route("excluir-salgado/{id:Guid}")]
+        public async Task<IActionResult> MudarStatusSalgado(Guid id)
+        {
+            var salgado = await _salgadosServices.ObterSalgadoPorId(id);
+
+            if (salgado == null) return NotFound("Salgado não encontrado");
+
+            salgado.DeletarSalgado();
+
+            await _salgadosServices.AtualizarSalgado(salgado);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("atualizar-status-pedido/{idPedido}/{status}")]
+        public async Task<IActionResult> AtualizarStatusPedido(Guid idPedido, string status)
+        {
+            var pedido = await _pedidoService.ObterPedidoPorId(idPedido);
+
+            if (pedido is null) return NotFound("Pedido não encontrado");
+
+            pedido.AlterarStatusPedido(status);
+
+            await _pedidoService.AtualizarPedido(pedido);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("obter-pedidos")]
+        public async Task<IActionResult> ObterPedidos()
+        {
+            var pedidos = await _pedidoService.ObterTodosPedidos();
+
+            return Ok(pedidos);
+        }
+    }
+}
---
+++ src/backend/WebApiSalgadosMeire/Controllers/ClientesController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiSalgadosMeire.Moldels;
+using WebApiSalgadosMeire.Services;
+using WebApiSalgadosMeire.ViewModels;
+
+namespace WebApiSalgadosMeire.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private readonly ClientesService _clienteServices;
+        private readonly PedidoService _pedidoService;
+        private readonly ItensPedidoService _itensPedidoService;
+
+        public ClientesController(ClientesService clienteService, PedidoService pedidoService, ItensPedidoService itensPedidoService)
+        {
+            _clienteServices = clienteService;
+            _pedidoService = pedidoService;
+            _itensPedidoService = itensPedidoService;
+        }
+
+        [HttpPost]
+        [Route("confirmar-dados-cliente")]
+        public async Task<IActionResult> ConfirmarDadosCliente(ClienteViewModel cliente)
+        {
+            var clienteExistente = await _clienteServices.ObterClientePorTelefone(cliente.Telefone);
+
+            if (clienteExistente is not null)
+            {
+                clienteExistente.AtualizarCliente(cliente.Nome, cliente.Endereco, cliente.Telefone);
+
+                await _clienteServices.AtualizarCliente(clienteExistente);
+
+                return Ok(clienteExistente);
+            }
+
+            var newCliente = new Cliente(cliente.Nome, cliente.Endereco, cliente.Telefone);
+
+            await _clienteServices.AdicionarCliente(newCliente);
+
+            var clienteCriado = await _clienteServices.ObterClientePorTelefone(cliente.Telefone);
+
+            return Ok(clienteCriado);
+        }
+
+        [HttpPost]
+        [Route("criar-pedido")]
+        public async Task<IActionResult> FazerPedido(PedidoViewModel model)
+        {
+            var novoPedido = new Pedido(model.ClienteId, model.DataPedido, model.FormaPagamento, model.TrocoPara);
+
+            var listaItensPedido = new List<ItensPedido>();
+
+            foreach (var item in model.ItensPedidos)
+            {
+                var novoItem = new ItensPedido(novoPedido.Id, item.SalgadoId, item.Quantidade, item.ValorUnitarioNaCompra, item.Observacoes);
+                listaItensPedido.Add(novoItem);
+            }
+
+            await _pedidoService.AdicionarPedido(novoPedido, listaItensPedido);
+
+            return Ok();
+
+        }
+
+        [HttpGet]
+        [Route("meus-pedidos/{id:Guid}")]
+        public async Task<IActionResult> ObterMeusPedidos(Guid id)
+        {
+            var pedidos = await _pedidoService.ObterPedidosPorClienteId(id);
+
+            return Ok(pedidos);
+
+        }
+
+        [HttpGet]
+        [Route("meu-login/{telefone}")]
+        public async Task<IActionResult> ObterMeusDados(string telefone)
+        {
+            var clienteExistente = await _clienteServices.ObterClientePorTelefone(telefone);
+
+            if (clienteExistente is not null)
+            {
+                return Ok(new { cadastrado = true, dados = clienteExistente });
+            }
+
+            return Ok(new { cadastrado = false, dados = clienteExistente });
+
+        }
+    }
+}
