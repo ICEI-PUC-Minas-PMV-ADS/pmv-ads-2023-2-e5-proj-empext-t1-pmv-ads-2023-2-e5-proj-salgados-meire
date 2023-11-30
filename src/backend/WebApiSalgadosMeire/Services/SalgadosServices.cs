8e561c21619b94c921a4ccf9b390f06ad520450c
Raíssa Queiroz <gnr0034@gmail.com>
2023-11-30 10:22:41 -0300

    services

---
+++ src/backend/WebApiSalgadosMeire/Services/SalgadosServices.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using WebApiSalgadosMeire.DataBase;
+using WebApiSalgadosMeire.Moldels;
+
+namespace WebApiSalgadosMeire.Services
+{
+    public class SalgadosServices
+    {
+        private readonly SalgadosMeireContext _context;
+
+        public SalgadosServices(SalgadosMeireContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Salgado>> ObterTodosSalgados()
+        {
+            return await _context.Salgados.Where(x => x.Ativo).ToListAsync();
+        }
+
+        public async Task<Salgado?> ObterSalgadoPorId(Guid id)
+        {
+            return await _context.Salgados.AsNoTracking().Where(x => x.Id.Equals(id)).FirstOrDefaultAsync();
+        }
+
+        public async Task AdicionarSalgado(Salgado salgado)
+        {
+            _context.Salgados.Add(salgado);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AtualizarSalgado(Salgado salgado)
+        {
+            _context.Salgados.Update(salgado);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
